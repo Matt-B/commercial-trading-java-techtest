4a55ca710cb8d25793bbc36c311862ccd671d6fc
Matt Bailey <matt@matt-bailey.co.uk>
2024-01-03 15:18:03 +0000

    Removed unused imports

--- src/main/java/com/global/commtech/test/anagramfinder/AnagramFileParser.java
+++ src/main/java/com/global/commtech/test/anagramfinder/AnagramFileParser.java
@@ -1,6 +1,5 @@
 package com.global.commtech.test.anagramfinder;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
--- src/test/java/com/global/commtech/test/anagramfinder/AnagramAggregatorTest.java
+++ src/test/java/com/global/commtech/test/anagramfinder/AnagramAggregatorTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
